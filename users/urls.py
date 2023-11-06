913b9497f3155dcea20923e11d49325f409c845a
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-11-07 02:03:41 +0330

    create user profile

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('', views.profiles_view, name='profiles'),
+    path('profile/<str:pk>/', views.user_profile_view, name='user_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .models import Profile
 
 
 def profiles_view(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {
+        'profiles': profiles
+    }
+    return render(request, 'users/profiles.html', context)
+
+
+def user_profile_view(request, pk):
+    profile = Profile.objects.get(id=pk)
+    top_skills = profile.skill_set.exclude(description__exact="")
+    other_skills = profile.skill_set.filter(description="")
+
+    context = {
+        'profile': profile,
+        'top_skills': top_skills,
+        'other_skills': other_skills
+    }
+    return render(request, 'users/user-profile.html', context)
